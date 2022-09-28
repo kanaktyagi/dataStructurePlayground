0416e34b868cd0da779024ef57a9e391a27eadfc
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-09-28 21:56:53 +0530

    Solved the maxSubArraySum using sliding window approach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { maxSubArray } from './MaxSubArray';
 
 function App() {
+  const result = maxSubArray([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>Max Array Sum :{result}</div>
     </div>
   );
 }
